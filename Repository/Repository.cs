f41cb04218acfa14bdd5912b5969bd743747492a
Moisés Mariano <moisesduartem@gmail.com>
2021-05-15 22:00:06 -0300

    Implementa interface IRepository na classe Repository

---
+++ Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Macoratti.RepositoryPattern.Api.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Macoratti.RepositoryPattern.Api.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public IEnumerable<T> Get()
+        {
+            return _context.Set<T>().AsEnumerable<T>();
+        }
+
+        public IEnumerable<T> Get(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().Where(predicate).AsEnumerable<T>();
+        }
+
+        public T GetById(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().SingleOrDefault(predicate);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.Set<T>().Update(entity);
+        }
+    }
+}
