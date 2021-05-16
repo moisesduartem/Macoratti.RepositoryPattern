58d636fc5637949005ba07fbf7cf3cf3dc51af4d
Moisés Mariano <moisesduartem@gmail.com>
2021-05-16 11:39:21 -0300

    Cria classe UnitOfWork e implementa IUnitOfWork

---
+++ Repository/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using Macoratti.RepositoryPattern.Api.Context;
+using Macoratti.RepositoryPattern.Api.Domain;
+using System;
+
+namespace Macoratti.RepositoryPattern.Api.Repository
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private AppDbContext _context;
+        private Repository<Cliente> _clienteRepository;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<Cliente> ClienteRepository
+        {
+            get {
+                return _clienteRepository = _clienteRepository ?? new Repository<Cliente>(_context);
+            }
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
