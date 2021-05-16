88550ca5188e032f8f96dfea889576f8637e4ced
Moisés Mariano <moisesduartem@gmail.com>
2021-05-16 12:09:45 -0300

    Divide solution em 2 camadas: DataAccess e BusinessLogic

---
+++ BusinessLogic/IClienteRepository.cs
@@ -0,0 +1,11 @@
+﻿using Macoratti.RepositoryPattern.Api.Domain;
+using Macoratti.RepositoryPattern.Api.Repository;
+using System.Collections.Generic;
+
+namespace BusinessLogic
+{
+    public interface IClienteRepository : IRepository<Cliente>
+    {
+        IEnumerable<Cliente> GetClientesPorNome();
+    }
+}
